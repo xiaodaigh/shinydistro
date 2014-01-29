4830dfea98eca51a938e5a018830c301302cc4f7
ZJ <zhuojia.dai@gmail.com>
2014-01-29 18:30:31 +0800

    Create runShinyApp.r

---
+++ windows/runShinyApp.r
@@ -0,0 +1,3 @@
+options(browser = "../../../../Apps/GoogleChromePortable/GoogleChromePortable.exe")
+.libPaths("../library")
+shiny::runApp("../../../../Apps/your_app/Shiny/",port=8888,launch.browser=TRUE)
