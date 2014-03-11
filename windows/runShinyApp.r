c699267ee17e76f60e71c35b20a22ee06f5b3fa8
ZJ <zhuojia.dai@gmail.com>
2014-03-11 13:11:14 +0800

    Update runShinyApp.r

--- windows/runShinyApp.r
+++ windows/runShinyApp.r
@@ -1,3 +1,5 @@
-options(browser = "./GoogleChromePortable/GoogleChromePortable.exe")
 .libPaths("./R-Portable/App/R-Portable/library")
+# you need the full path to portable chrome
+browser.path <- file.path(getwd(),"GoogleChromePortable/GoogleChromePortable.exe")
+options(browser = "./GoogleChromePortable/GoogleChromePortable.exe")
 shiny::runApp("./yourApp/Shiny/",port=8888,launch.browser=TRUE)
